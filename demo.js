7d6665eeaf578acd839d596e72b3689b03574b7a
nikhilzende895 <142150240+nikhilzende895@users.noreply.github.com>
2023-08-13 20:15:55 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,8 +1,1 @@
-let a = prompt("What is your age");
-a = Number(a);
-
-if(a > 17) {
-  alert("yo Are a Aligable for voiting");
-} else {
-  alert("You are a not a aligalbe for voiting"):
-}
+prompt("What is your age");
